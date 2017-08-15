ce7668d3f3b0ededdea2cd177a9aeeedc1b9cccb
sophdaly <sophdaly@gmail.com>
2017-08-15 11:26:13 +0100

    Updated implementation and added TensorBoard

--- src/mnist/conv_mnist.py
+++ src/mnist/conv_mnist.py
@@ -34,62 +34,111 @@ def conv2d(x, W):
     return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
 
-def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1],
-                          padding='SAME')
+def variable_summaries(var, name):
+    mean = tf.reduce_mean(var)
+    tf.summary.scalar(name + '/mean', mean)
+    stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+    tf.summary.scalar(name + '/stddev', stddev)
 
 
-def conv2d_layer(input_tensor, filter_size, no_filters, act=tf.nn.relu):
+def activation_summaries(x, name):
+    tf.summary.scalar(name + '/sparsity', tf.nn.zero_fraction(x))
+    tf.summary.histogram(name + '/activations', x)
+    variable_summaries(x, name)
+
+
+def conv2d_layer(layer_name, input_tensor, filter_size, no_filters,
+                 act=tf.nn.relu, summaries=False):
     '''
-    Implement 2D convolution, non-linearize with ReLU and apply 2x2 max pooling
+    Implement 2D convolution, non-linearize with ReLU
     '''
-    input_dim = input_tensor.get_shape()[-1].value
-    weight = weight_variable([filter_size, filter_size, input_dim,
-                              no_filters])
-    bias = bias_variable([no_filters])
-    activation = act(conv2d(input_tensor, weight) + bias, "activation")
-    activation = max_pool_2x2(activation)
+    with tf.name_scope(layer_name):
+        input_dim = input_tensor.get_shape()[-1].value
 
-    return activation
+        with tf.variable_scope(layer_name):
+            weight = weight_variable([filter_size, filter_size,
+                                     input_dim, no_filters])
+            bias = bias_variable([no_filters])
+            if summaries:
+                variable_summaries(weight, layer_name + '/weight')
+                variable_summaries(bias, layer_name + '/bias')
+
+        with tf.name_scope('activations'):
+            preactivate = tf.nn.bias_add(conv2d(input_tensor, weight), bias)
+            activation = act(preactivate, "activation")
+            if summaries:
+                activation_summaries(activation, layer_name)
+
+            return activation
 
 
-def fc_layer(input_tensor, no_filters, act=tf.nn.relu, dropout=None):
+def max_pool2d_layer(layer_name, input_tensor, pool_size, stride):
+    '''
+    Apply 2x2 max pooling
+    '''
+    with tf.name_scope(layer_name):
+        pooled = tf.nn.max_pool(
+            input_tensor, ksize=[1, pool_size, pool_size, 1],
+            strides=[1, stride, stride, 1], padding='SAME'
+        )
+        return pooled
+
+
+def fc_layer(layer_name, input_tensor, no_filters, act=tf.nn.relu,
+             dropout=None, summaries=False):
     '''
     Flatten input_tensor if needed, non-linearize with ReLU and optionally
     apply dropout
     '''
-    # Reshape input tensor to flatten tensor if needed
-    input_shape = input_tensor.get_shape()
-    if len(input_shape) == 4:
-        input_dim = np.int(np.product(input_shape[1:]))
-        input_tensor = tf.reshape(input_tensor, [-1, input_dim])
-    elif len(input_shape) == 2:
-        input_dim = input_shape[-1].value
-    else:
-        raise RuntimeError('ERROR Input Tensor Shape: {}'.format(input_shape))
+    with tf.name_scope(layer_name):
+        # Reshape input tensor to flatten tensor if needed
+        input_shape = input_tensor.get_shape()
+        if len(input_shape) == 4:
+            input_dim = np.int(np.product(input_shape[1:]))
+            input_tensor = tf.reshape(input_tensor, [-1, input_dim])
+        elif len(input_shape) == 2:
+            input_dim = input_shape[-1].value
+        else:
+            raise RuntimeError('Input Tensor Shape: {}'.format(input_shape))
 
-    weight = weight_variable([input_dim, no_filters])
-    bias = bias_variable([no_filters])
+        with tf.variable_scope(layer_name):
+            weight = weight_variable([input_dim, no_filters])
+            bias = bias_variable([no_filters])
+            if summaries:
+                variable_summaries(weight, layer_name + '/weight')
+                variable_summaries(bias, layer_name + '/bias')
 
-    preactivate = tf.nn.bias_add(tf.matmul(input_tensor, weight), bias)
-    activation = act(preactivate, "activation")
+        with tf.name_scope('activations'):
+            preactivate = tf.nn.bias_add(tf.matmul(input_tensor, weight), bias)
+            activation = act(preactivate, "activation")
+            if dropout is not None:
+                activation = tf.nn.dropout(activation, dropout)
+            if summaries:
+                activation_summaries(activation, layer_name)
 
-    if dropout is not None:
-        activation = tf.nn.dropout(activation, dropout)
-
-    return activation
+            return activation
 
 
-def deep_nn(x, keep_prob):
-
+def inference(x, keep_prob, summaries):
+    '''
+    Build network graph to compute output predictions for images
+    '''
     # Reshape x to 4d tensor
     # [BATCH_SIZE, 784] -> [BATCH_SIZE, width, height, no_color_channels]
     x_image = tf.reshape(x, [-1, 28, 28, 1])
 
-    out_1 = conv2d_layer(input_tensor=x_image, filter_size=5, no_filters=32)
-    out_2 = conv2d_layer(input_tensor=out_1, filter_size=5, no_filters=64)
-    out_3 = fc_layer(input_tensor=out_2, no_filters=1024, dropout=keep_prob)
-    y_pred = fc_layer(input_tensor=out_3, no_filters=10, act=tf.identity)
+    out = conv2d_layer(input_tensor=x_image, filter_size=5, no_filters=32,
+                       layer_name='conv_1', summaries=summaries)
+    out = max_pool2d_layer(input_tensor=out, pool_size=2, stride=2,
+                           layer_name='max_pool_1')
+    out = conv2d_layer(input_tensor=out, filter_size=5, no_filters=64,
+                       layer_name='conv_2', summaries=summaries)
+    out = max_pool2d_layer(input_tensor=out, pool_size=2, stride=2,
+                           layer_name='max_pool_2')
+    out = fc_layer(input_tensor=out, no_filters=1024, dropout=keep_prob,
+                   layer_name='fc_1', summaries=summaries)
+    y_pred = fc_layer(input_tensor=out, no_filters=10, act=tf.identity,
+                      layer_name='fc_2', summaries=summaries)
 
     return y_pred
 
@@ -106,21 +155,30 @@ def main(_):
     y_ = tf.placeholder(tf.float32, shape=[None, 10])
     keep_prob = tf.placeholder(tf.float32)
 
-    # Build the graph for deep net
-    y_pred = deep_nn(x, keep_prob)
+    # Build graph for deep net
+    y_pred = inference(x, keep_prob, FLAGS.tensorboard)
 
     # Loss fn
     cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
         labels=y_, logits=y_pred))
 
     # Train
-    train_step = tf.train.AdamOptimizer(LEARNING_RATE).minimize(cross_entropy)
+    train_op = tf.train.AdamOptimizer(LEARNING_RATE).minimize(cross_entropy)
 
     # Evaluation fns
     correct_prediction = tf.equal(tf.argmax(y_pred, 1), tf.argmax(y_, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    accuracy_op = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    # Monitoring
+    tf.summary.scalar('loss', cross_entropy)
+    tf.summary.scalar('accuracy', accuracy_op)
 
     with tf.Session() as sess:
+        summary_op = tf.summary.merge_all()
+        train_writer = tf.summary.FileWriter(FLAGS.log_dir + '/train',
+                                             sess.graph)
+        test_writer = tf.summary.FileWriter(FLAGS.log_dir + '/test',
+                                            sess.graph)
 
         sess.run(tf.global_variables_initializer())
         no_vals = mnist.validation.labels.shape[0]
@@ -128,42 +186,43 @@ def main(_):
         # Training cycle
         t0 = time.time()
         for i in range(5000):
+            # Train and record train set summaries
             batch_xs, batch_ys = mnist.train.next_batch(BATCH_SIZE)
-            train_step.run(feed_dict={x: batch_xs, y_: batch_ys,
-                           keep_prob: 0.5})
-
-            if i % 10 == 0:
-                print("Iter: {}".format(i))
+            summary, _ = sess.run([summary_op, train_op], feed_dict={
+                                  x: batch_xs, y_: batch_ys, keep_prob: 0.5
+                                  })
+            train_writer.add_summary(summary, i)
 
             if i % DISPLAY_STEP == 0:
-                print("Evaluating...")
-                # Subsample eval set (for speed)
-                # Compute output, and compute metrics
+                # Evaluate accuracy on test set and record summaries
+                # Subsample test set (for speed)
                 mean_batch_time = (time.time() - t0) / 100.0
                 t1 = time.time()
                 sample = np.random.choice(range(no_vals), 500, replace=False)
 
-                acc = accuracy.eval(feed_dict={
-                                    x: mnist.validation.images[sample],
-                                    y_: mnist.validation.labels[sample],
-                                    keep_prob: 1.0
-                                    })
-                loss = cross_entropy.eval(feed_dict={
-                                          x: mnist.test.images,
-                                          y_: mnist.test.labels,
-                                          keep_prob: 1.0
-                                          })
+                summary, acc, loss = sess.run(
+                    [summary_op, accuracy_op, cross_entropy],
+                    feed_dict={x: mnist.validation.images[sample],
+                               y_: mnist.validation.labels[sample],
+                               keep_prob: 1.0}
+                )
+                test_writer.add_summary(summary, i)
 
-                print("Iter: {}, Loss: {}, Accuracy: {}, train batch time: \
-                    [{:.2f} s], eval_time:[{:.2f} s]".format(
-                    i, loss, acc, mean_batch_time, time.time() - t1))
+                print(("Iter: {}, Loss: {}, Accuracy: {} "
+                      "[train batch time: {:.2f}s, eval time: {:.2f}s]")
+                      .format(i, loss, acc, mean_batch_time, time.time() - t1))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data', type=str,
-                        default='data/mnist_data',
+    parser.add_argument('--data', type=str, default='data/mnist_data',
                         help='Path to input data')
+    parser.add_argument('--tensorboard', action='store_true',
+                        help='TensorBoard on/off ')
+    parser.add_argument('--log_dir', type=str,
+                        default='logs/mnist/conv_mnist',
+                        help='TensorBoard Summaries log directory')
 
     FLAGS, unparsed = parser.parse_known_args()
+
     tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
